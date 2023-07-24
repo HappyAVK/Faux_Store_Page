4e54bb0e43b9e1ce2583e4a4d91cf9b35852a75e
Alex <alexlsouthall@yahoo.com>
2023-07-24 21:35:18 +0100

    Set Debug to 'True'

--- artist_portfolio/settings.py
+++ artist_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k^y$*1fys3(m--360@#-^-!6-bor!s1%+u9l5#_8-y&%73row^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
