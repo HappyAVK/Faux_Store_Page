b778207c9f8a11f073fd9ce15bbe03c5207e6fe6
Alex <alexlsouthall@yahoo.com>
2023-07-25 13:30:39 +0100

    updated settings

--- artist_portfolio/settings.py
+++ artist_portfolio/settings.py
@@ -131,5 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+DISABLE_COLLECTSTATIC = 1
 ##import django_heroku
 ##django_heroku.settings(locals())
